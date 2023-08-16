b6b948029c87166bf984d1ebd868ce7bac3976ca
billal-webdev <billal-webdev@gmail.com>
2023-08-16 23:09:37 +0600

    color picker added for text

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,174 @@
-alert()
+//get the selected text
+const textArea = document.getElementById("text-area");
+const colorContainer = document.getElementById("text-color-selector");
+let selectedTxt = "";
+document.addEventListener('mouseup', event => {  
+       if(window.getSelection().toString().length){
+         selectedTxt = window.getSelection().toString();
+       } 
+})
+const textColors = ['#06D6A0', '#1B9AAA', '#ef476f', '#FFC43D', '#FFC94F', '#264653', '#344e41', '#fb8500', '#fb5607', '#e29578', '#00b4d8', '#e56b6f', '#354f52', '#004b23', '#04395e', '#1b2021', '#326771', '#1e352f', '#3f4f35', '#9bb1ff', '#e53d00', '#df2935', '#fdca40', '#540d6e', '#3bceac', '#7dcfb6', '#080708', '#0ead69']
+// clipboard
+document.getElementById("clipboard").addEventListener('click', (ev)=>{
+    if(ev.target.classList.contains('copy') || ev.target.classList.contains('fa-copy') || ev.target.classList.contains('copy-text')){
+    //   copy text 
+    const text = textArea.innerText;
+    navigator.clipboard.writeText(text)
+    .then(()=>alert("Copied text"))
+    .catch(error => console.log('Failed to copy==> ', error.message))
+    }else if(ev.target.classList.contains('cut') || ev.target.classList.contains('fa-scissors') || ev.target.classList.contains('cut-text')){
+        //   cut text 
+        const text = textArea.innerText;
+        navigator.clipboard.writeText(text)
+        .then(()=>alert("Cut text"))
+        .catch(error => console.log("Failed to cut======>", error.message))
+        textArea.innerHTML = "";
+    }else if(ev.target.classList.contains('paste') || ev.target.classList.contains('fa-clipboard') || ev.target.classList.contains('paste-text')){
+        //   paste text 
+        navigator.clipboard.readText()
+        .then(text =>{
+            textArea.innerText += text;
+        })
+    }
+})
+
+
+// font related features 
+const font = document.getElementById("font");
+let currentSize = 16;
+
+let formatedText = "";
+font.addEventListener('click', (ev)=>{
+    const wholeText = textArea.innerHTML;
+
+    if(ev.target.classList[1] === "fa-bold"){
+        if(selectedTxt){
+        formatedText = wholeText.replace(selectedTxt, `<span style="font-weight:bold">${selectedTxt}</span>`)
+        textArea.innerHTML = formatedText;
+        selectedTxt = "";
+        }else{
+            if(textArea.style.fontWeight){
+                textArea.style.fontWeight = "";
+                ev.target.classList.remove('active');
+            }else{
+                ev.target.classList.add('active');
+                textArea.style.fontWeight = "bold";
+            }
+        }
+    }else if(ev.target.classList[1] === "fa-italic"){
+        if(selectedTxt){
+        formatedText = wholeText.replace(selectedTxt, `<span style="font-style:italic">${selectedTxt}</span>`)
+        textArea.innerHTML = formatedText;
+        selectedTxt = "";
+        }else{
+            if(textArea.style.fontStyle){
+                textArea.style.fontStyle = "";
+                ev.target.classList.remove('active');
+            }else{
+                ev.target.classList.add('active');
+                textArea.style.fontStyle = "italic";
+            }
+        }
+    }else if(ev.target.classList[1] === "fa-underline"){
+        if(selectedTxt){
+            formatedText = wholeText.replace(selectedTxt, `<span style="text-decoration:underline">${selectedTxt}</span>`)
+            textArea.innerHTML = formatedText;
+            selectedTxt = "";
+            }else{
+                if(textArea.style.textDecoration){
+                    textArea.style.textDecoration = "";
+                    ev.target.classList.remove('active');
+                }else{
+                    ev.target.classList.add('active');
+                    textArea.style.textDecoration = "underline";
+                }
+            }
+    }else if(ev.target.classList[1] === "fa-strikethrough"){
+        if(selectedTxt){
+            formatedText = wholeText.replace(selectedTxt, `<strike>${selectedTxt}</strike>`)
+            textArea.innerHTML = formatedText;
+            selectedTxt = "";
+            }else{
+                if(textArea.style.textDecoration){
+                    textArea.style.textDecoration = "";
+                    ev.target.classList.remove('active');
+                }else{
+                    ev.target.classList.add('active');
+                    textArea.style.textDecoration = "line-through"
+                }
+            }
+    }else if(ev.target.classList.contains("fa-sort-up") || ev.target.classList.contains("increment-font") || ev.target.classList.contains("fa-up")){
+        currentSize++;
+        textArea.style.fontSize = `${currentSize}px`
+    }else if(ev.target.classList.contains("fa-sort-down") || ev.target.classList.contains("decrease-font") || ev.target.classList.contains("fa-down")){
+        currentSize--;
+        textArea.style.fontSize = `${currentSize}px`
+    }else if(ev.target.classList.contains('text-color')){
+        /*=========================TExt Color=======================*/
+        colorContainer.innerHTML = "";
+        textColors.forEach(color => {
+            colorContainer.innerHTML += `<div onclick='changeTextColor("${color}")' class="color-box" style="background:${color}"><div>`;
+        })
+    }
+})
+
+function changeTextColor(color){
+    textArea.style.color = color;
+}
+
+document.getElementById("font-family").addEventListener('change', ev=>{
+    const wholeText = textArea.innerHTML;
+    if(selectedTxt){
+        formatedText = wholeText.replace(selectedTxt, `<span style="font-family:${ev.target.value}">${selectedTxt}</span>`)
+        textArea.innerHTML = formatedText;
+        selectedTxt = "";
+        }else{
+            if(textArea.style.fontFamily){
+                textArea.style.fontFamily = "";
+            }else{
+                textArea.style.fontFamily = `${ev.target.value}`;
+            }
+        }
+})
+
+document.getElementById("font-size").addEventListener('change', ev=>{
+    const wholeText = textArea.innerHTML;
+    if(selectedTxt){
+        formatedText = wholeText.replace(selectedTxt, `<span style="font-size:${ev.target.value}px">${selectedTxt}</span>`)
+        textArea.innerHTML = formatedText;
+        selectedTxt = "";
+        }else{
+        textArea.style.fontSize = `${ev.target.value}px`;
+        }
+})
+
+
+
+
+
+// ===================================================================================color 
+const textColorWrapper = document.getElementById("text-color-wrapper")
+textColorWrapper.addEventListener('click', ev=>{
+    if(ev.target.classList.contains('text-color')){
+        colorContainer.classList.toggle('toggleTextColor')
+    }
+})
+
+//paragraph logics
+document.getElementById("paragraph").addEventListener('click', ev=>{
+    const aligns = document.querySelectorAll('.fa-align');
+    aligns.forEach(align => align.classList.remove('active'));
+    if(ev.target.classList.contains('fa-align-left')){
+        textArea.style.textAlign = "left";
+        ev.target.classList.add('active');
+    }else if(ev.target.classList.contains('fa-align-center')){
+        textArea.style.textAlign = "center";
+        ev.target.classList.add('active');
+    }else if(ev.target.classList.contains('fa-align-right')){
+        textArea.style.textAlign = "right";
+        ev.target.classList.add('active');
+    }else if(ev.target.classList.contains('fa-align-justify')){
+        textArea.style.textAlign = "justify";
+        ev.target.classList.add('active');
+    }
+})
